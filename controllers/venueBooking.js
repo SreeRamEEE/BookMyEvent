e177dbe3f402e7fe039605c40904115a8e7fff30
Sriramulu Gopu <sriram0411524@gmail.com>
2025-10-31 11:19:52 +0530

    get Bookings

--- controllers/venueBooking.js
+++ controllers/venueBooking.js
@@ -24,7 +24,56 @@ const bookVenue = async (req, res) => {
 const getVenueBookingsByUser = async (req, res) => {
   try {
     const { userId } = req.params;
-    const bookings = await VenueBooking.find({ bookedBy: userId });
+   const bookings = await VenueBooking.aggregate([
+      { $match: { bookedBy: new mongoose.Types.ObjectId(userId) } },
+      {
+        $lookup: {
+          from: 'registers',
+          localField: 'bookedBy',
+          foreignField: '_id',
+          as: 'register'
+        }
+      },
+       { $unwind: '$register' },
+      {
+        $lookup: {
+          from: 'venues',
+          localField: 'venueId',
+          foreignField: '_id',
+          as: 'venue'
+        },
+
+      },
+      { $unwind: '$venue' },
+      {
+        $lookup: {
+          from :'venueprices',
+          localField: 'venueId',
+          foreignField: 'venueId',
+          as: 'venuePrice'
+        }
+      },
+      { $unwind: '$venuePrice' },
+      { $project: {
+          _id: 1,
+          date: 1,
+          startTime: 1,
+          endTime: 1,
+          bookedBy: {
+            _id: '$register._id',
+            name: '$register.name',
+            mobile: '$register.mobile'
+          },
+          venue: {
+            _id: '$venue._id',
+            name: '$venue.name',
+            location: '$venue.location',
+             price: '$venuePrice.price'
+          },
+        
+        }
+      }
+    ]);
     res.status(200).json({ data: { bookings }, message: "User bookings retrieved successfully" });
   } catch (error) {
     console.error("Error fetching user bookings:", error);
@@ -32,7 +81,74 @@ const getVenueBookingsByUser = async (req, res) => {
   }
 };
 
+const getAllVenueBookings = async (req, res) => {
+  try {
+
+ 
+const bookings = await VenueBooking.aggregate([
+      {
+        $lookup: {
+          from: 'registers',
+          localField: 'bookedBy',
+          foreignField: '_id',
+          as: 'register'
+        }
+      },
+       { $unwind: '$register' },
+      {
+        $lookup: {
+          from: 'venues',
+          localField: 'venueId',
+          foreignField: '_id',
+          as: 'venue'
+        },
+
+      },
+      { $unwind: '$venue' },
+      {
+        $lookup: {
+          from :'venueprices',
+          localField: 'venueId',
+          foreignField: 'venueId',
+          as: 'venuePrice'
+        }
+      },
+      { $unwind: '$venuePrice' },
+      { $project: {
+          _id: 1,
+          date: 1,
+          startTime: 1,
+          endTime: 1,
+          bookedBy: {
+            _id: '$register._id',
+            name: '$register.name',
+            mobile: '$register.mobile'
+          },
+          venue: {
+            _id: '$venue._id',
+            name: '$venue.name',
+            location: '$venue.location',
+             price: '$venuePrice.price'
+          },
+        
+        }
+      }
+    ]);
+
+
+
+    // console.log(data);
+    res.status(200).json({ data: { bookings }, message: "All venue bookings retrieved successfully" });
+  } catch (error) {
+    console.error("Error fetching all venue bookings:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+
 module.exports = {
   bookVenue,
-  getVenueBookingsByUser
+  getVenueBookingsByUser,
+  getAllVenueBookings
 };
--- routes/bookVenue.js
+++ routes/bookVenue.js
@@ -3,4 +3,5 @@ const express = require('express');
 const router = express.Router();
 router.post('/book-venue', venueBookingController.bookVenue);
 router.get('/user-bookings/:userId', venueBookingController.getVenueBookingsByUser);
+router.get('/all-bookings', venueBookingController.getAllVenueBookings);
 module.exports = router;
